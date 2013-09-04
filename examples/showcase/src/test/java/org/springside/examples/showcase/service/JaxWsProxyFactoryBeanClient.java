4943b2e244d90445d40a3800f7ee30d14af496ed
jnuc093 <jnuc093@gmail.com>
2013-09-04 20:28:28 +0800

    webservice例子

--- examples/showcase/src/test/java/org/springside/examples/showcase/service/JaxWsProxyFactoryBeanClient.java
+++ examples/showcase/src/test/java/org/springside/examples/showcase/service/JaxWsProxyFactoryBeanClient.java
@@ -7,8 +7,8 @@ import org.springside.examples.showcase.webservice.soap.response.SearchMessageRe
 public class JaxWsProxyFactoryBeanClient {
 	public static void main(String[] arg){
         JaxWsProxyFactoryBean soapFactoryBean = new JaxWsProxyFactoryBean();
-        
-		soapFactoryBean.setAddress("http://127.0.0.1:8080/showcase/soap/waterservice");    // 这个就是第一个工程完成后所测试的地址。
+        //http://192.157.242.30:8080/showcase/soap/waterservice?wsdl
+		soapFactoryBean.setAddress("http://192.157.242.30:8080/showcase/soap/waterservice");    // 这个就是第一个工程完成后所测试的地址。
         soapFactoryBean.setServiceClass(WaterWebService.class);
         Object o = soapFactoryBean.create();
         WaterWebService waterWebService = (WaterWebService) o;
